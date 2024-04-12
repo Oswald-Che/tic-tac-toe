1403d65f6d0859a0384f4ae19903142e40ac6b96
Oswald-Che <cheoswald@yahoo.com>
2024-04-12 16:57:03 +0100

    Add display board method to board class

--- board.rb
+++ board.rb
@@ -4,4 +4,20 @@ class Board
     @board = Array.new(9, ' ')
   end
 
+  def display_tutorial
+    puts " 1 | 2 | 3 \n-----------\n 4 | 5 | 6 \n-----------\n 7 | 8 | 9 "
+  end
+
+  def display_board
+    @board.each_slice(3) do |a, b, c|
+      puts " #{a} | #{b} | #{c} "
+      puts '-----------'
+    end
+  end
+
 end
+
+board = Board.new
+
+board.display_tutorial
+board.display_board
